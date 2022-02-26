dd44656956818cb4be77bcfdfc2357439c87f0b6
Saar Amrani <saar120@gmail.com>
2022-02-26 17:26:38 +0200

    max char completed

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,12 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const charMap = {};
+  for (char of str) {
+    charMap[char] ? charMap[char]++ : (charMap[char] = 1);
+  }
+  return Object.keys(charMap).reduce((max, next) => (charMap[max] > charMap[next] ? max : next));
+}
 
 module.exports = maxChar;
